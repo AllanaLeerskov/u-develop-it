7045b521ed946a71c5d7e595752ae10259a2eb40
Elektra <allanaleerskov@gmail.com>
2022-01-23 22:38:14 -0700

    add mysql info add db query

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,12 +7,30 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //your mysql username
+        user: 'root',
+        //your mysql password
+        password: 'Family09!1',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
 //get route
     // app.get('/', (req, res) => {
     //     res.json({
     //         message: 'Hello World'
     //     });
     // });
+
+//return data from candidates table
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 //default response for any other request (Not Found)
 app.use((req, res) => {
